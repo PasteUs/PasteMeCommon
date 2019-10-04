8689d9a18228eba4cef4e187f26d34472e389884
1059438020 <1502010425@s.upc.edu.cn>
2019-10-04 19:39:40 +0800

    去掉接口上的 @Service

--- src/main/java/cn/pasteme/common/manager/PermanentManager.java
+++ src/main/java/cn/pasteme/common/manager/PermanentManager.java
@@ -1,11 +1,8 @@
 package cn.pasteme.common.manager;
 
-import org.springframework.stereotype.Service;
-
 /**
  * @author Lucien
  * @date 2019/10/01 12:32
  */
-@Service
 public interface PermanentManager extends PasteManager {
 }
--- src/main/java/cn/pasteme/common/manager/TemporaryManager.java
+++ src/main/java/cn/pasteme/common/manager/TemporaryManager.java
@@ -1,11 +1,8 @@
 package cn.pasteme.common.manager;
 
-import org.springframework.stereotype.Service;
-
 /**
  * @author Lucien
  * @date 2019/10/01 12:35
  */
-@Service
 public interface TemporaryManager extends PasteManager {
 }
